6b6b5522e203e0bd3ffe2e88b1d35cb8da53cfac
returnaction <obergannv@gmail.com>
2023-12-03 02:50:21 -0500

    Add model Task

---
+++ backend/backend/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CompletedAt { get; set; }
+
+    }
+}
